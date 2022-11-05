124bcd1e7410b20aa767ae4fd8e29190066c580d
Datta Sandu Khatik <107250865+dattacodex@users.noreply.github.com>
2022-11-05 16:57:32 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
     METERED_DOMAIN: process.env.METERED_DOMAIN || "https://herokuvideocall4.herokuapp.com/",
-    METERED_SECRET_KEY: process.env.METERED_SECRET_KEY || "",
+    METERED_SECRET_KEY: process.env.METERED_SECRET_KEY || "123456",
     port: process.env.PORT || 4000
 }
